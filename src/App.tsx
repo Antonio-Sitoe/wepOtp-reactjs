3961815e17c16aefd6d767467d600824cbcbb058
Antonio Sitoe <antoniositoehl@gmail.com>
2024-09-17 10:35:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    const handleCopy = (event: ClipboardEvent) => {
+      try {
+        // @ts-ignore
+        const clipboardData = event.clipboardData || window.clipboardData
+        const copiedText = clipboardData?.getData('text') || ''
+        console.log('Copied content:', copiedText)
+        setMessage(copiedText)
+      } catch (err) {
+        console.error('Failed to read clipboard contents:', err)
+      }
+    }
+
+    // Adiciona o listener para o evento 'copy'
+    document.addEventListener('copy', handleCopy)
+
+    // Limpa o event listener ao desmontar o componente
+    return () => {
+      document.removeEventListener('copy', handleCopy)
+    }
+  }, [])
 
   useEffect(() => {
     if ('OTPCredential' in window) {
@@ -41,15 +61,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <form action="">
           <input
@@ -61,13 +72,9 @@ function App() {
             autoComplete="one-time-code"
           />
         </form>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h3>Clipboard Monitor</h3>
+        <p>Copied content: {message || 'Nothing copied yet.'}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
